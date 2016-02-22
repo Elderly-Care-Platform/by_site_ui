994aa47ce9807cee1b6afdc4fe6d8bad5ac99a61
Aparna <aparna.r@beautifulyears.com>
2016-02-22 14:24:46 +0530

    Community detailed related post

--- src/webapp/app/components/discussDetail/discussDetailLeftCtrl.js
+++ src/webapp/app/components/discussDetail/discussDetailLeftCtrl.js
@@ -25,7 +25,7 @@ define(['byApp', 'byUtil', 'discussService'], function(byApp, byUtil, discussSer
 							}
 						}
 						$scope.header1 = "Also by ";
-						$scope.authorName = BY.byUtil.validateUserName($scope.discuss.username);
+						$scope.authorName = BY.byUtil.validateUserName($scope.discuss.username) + ":";
 					},
 					function(error){
 						console.log(error);
@@ -101,8 +101,8 @@ define(['byApp', 'byUtil', 'discussService'], function(byApp, byUtil, discussSer
 					function(response){
 						$scope.articlesByUser = response.data.content;
 						$scope.articlesByUserFormat = discussServiceFilter.formatData($scope.articlesByUser);
-						$scope.header1 = "Related Post by";
-						$scope.authorName = BY.byUtil.validateUserName($scope.discuss.username);;
+						$scope.header1 = "Related Post";
+						$scope.authorName = "";
 					},
 					function(error){
 						console.log(error);
