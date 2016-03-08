603517d28ee8ccfb3b5656462abf1896cbdfdbc7
Aparna <aparna.r@beautifulyears.com>
2016-03-08 15:16:24 +0530

    contact us ctrl path change in home controller

--- src/webapp/app/components/home/homeController.js
+++ src/webapp/app/components/home/homeController.js
@@ -4,7 +4,7 @@
 //home
 define(['byApp', 'byUtil', 'homePromoController',
         'userTypeConfig', 
-        'byEditor', 'menuConfig', 'editorController', 'userValidation', 'productController', 'contactUsCtrl'],
+        'byEditor', 'menuConfig', 'editorController', 'userValidation', 'productController', 'app/shared/footer/contactUsController'],
     function (byApp, byUtil, homePromoController, userTypeConfig, byEditor, menuConfig, editorController, userValidation, productController, contactUsCtrl) {
         function BYHomeController($scope, $rootScope, $routeParams, $location, UserValidationFilter, SessionIdService) {
             $scope.homeSectionConfig = BY.config.menu.home;
@@ -76,10 +76,10 @@ define(['byApp', 'byUtil', 'homePromoController',
 
             $scope.exitEditorDiscuss = function (type, event) {
                 event.stopPropagation();
-                $(".by_homeEditor").animate({width: '50%', height: '171px', marginBottom: '20px'}, "500");
+                $(".by_homeEditor").animate({width: '100%', height: '115px', marginBottom: '20px'}, "500");
                 $(".by_homeEditorShow").hide();
                 $(".by_homeTextareaShow").show();
-                $(".by_homeTalk").animate({width: '49%'}, "500");
+                $(".by_homeTalk").animate({width: '100%'}, "500");
                 $(".by_homeEditorShow").slideUp("100", function () {
                     BY.byEditor.removeEditor();
                     //$route.reload();
