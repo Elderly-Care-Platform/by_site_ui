a74c0541a9a216808f79a2e4601347f44d9de47a
Aparna <aparna.r@beautifulyears.com>
2016-04-20 16:18:16 +0530

    Classes & activities ui

--- src/webapp/app/components/classes-activities/classes-activities.js
+++ src/webapp/app/components/classes-activities/classes-activities.js
@@ -3,27 +3,24 @@ define(['byApp', 'byUtil'], function(byApp, byUtil) {
     
         $scope.telNo = BY.config.constants.byContactNumber;
 
-        /*var init = initialize();
+        var init = initialize();
         function initialize() {
             $(".by_header").removeClass("by_header_image"); 
             $(".by_header").addClass("by_headerBoder");
-
             angular.element($window).bind("scroll", function () {
                 var headerHeight = $(".by_header").height();
                 if ((document.body.scrollTop || document.documentElement.scrollTop || window.pageYOffset) >= headerHeight) {
                     BY.byUtil.changeHeaderImage();
-                    $(".by_header").removeClass("by_header_image"); 
-                    $(".by_header").addClass("by_homeModiBannerHeader");
+                    $(".by_header").addClass("by_header_image"); 
                     $(".by_header").removeClass("by_headerBoder");
                 } else {
                      BY.byUtil.changeHeaderImage();
-                    $(".by_header").removeClass("by_header_image");          
-                    $(".by_header").removeClass("by_homeModiBannerHeader");
+                    $(".by_header").removeClass("by_header_image"); 
                     $(".by_header").addClass("by_headerBoder");
                 }
                     
             });
-        }*/
+        }
 
     }
 
