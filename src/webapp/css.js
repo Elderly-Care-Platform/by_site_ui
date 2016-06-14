2830daac05287457df42070294c43950a326cf56
Aparna <aparna.r@beautifulyears.com>
2016-06-14 14:46:10 +0530

    css changes

--- src/webapp/css.js
+++ src/webapp/css.js
@@ -1,20 +1,14 @@
-function loadjscssfile(filename, filetype){
-    if (filetype=="css"){ //if filename is an external CSS file
-        var fileref=document.createElement("link")
-        fileref.setAttribute("rel", "stylesheet")
-        fileref.setAttribute("type", "text/css")
-        fileref.setAttribute("href", filename)
-    }
-    if (typeof fileref!="undefined")
-        document.getElementsByTagName("head")[0].appendChild(fileref)
-}
- 
-loadjscssfile("lib/unify/plugins/bootstrap/css/bootstrap.min.css?versionTimeStamp=%PROJECT_VERSION%", "css") ////dynamically load and add this .css file
-loadjscssfile("lib/jqueryPlugins/jquery-ui-1.11.4.custom/jquery-ui.min.css?versionTimeStamp=%PROJECT_VERSION%", "css")
-loadjscssfile("lib/unify/plugins/bootstrap/css/bootstrap-toggle.min.css?versionTimeStamp=%PROJECT_VERSION%", "css")
-loadjscssfile("lib/tagmanager-master/tagmanager.css?versionTimeStamp=%PROJECT_VERSION%", "css")
-loadjscssfile("lib/unify/plugins/animate.css?versionTimeStamp=%PROJECT_VERSION%", "css")
-loadjscssfile("lib/unify/plugins/line-icons/line-icons.css?versionTimeStamp=%PROJECT_VERSION%", "css")
-loadjscssfile("lib/unify/plugins/font-awesome/css/font-awesome.min.css?versionTimeStamp=%PROJECT_VERSION%", "css")
-loadjscssfile("lib/unify/plugins/flexslider/flexslider.css?versionTimeStamp=%PROJECT_VERSION%", "css")
-loadjscssfile("lib/unify/plugins/parallax-slider/css/parallax-slider.css?versionTimeStamp=%PROJECT_VERSION%", "css")
+ // <!-- Third party JS files -->
+    document.write('<script type="text/javascript" src="lib/unify/plugins/bootstrap/js/bootstrap.min.js?versionTimeStamp=%PROJECT_VERSION%"></script>');
+    document.write('<script type="text/javascript" src="lib/unify/unify_app.js?versionTimeStamp=%PROJECT_VERSION%"></script>');
+    document.write('<script type="text/javascript" src="lib/unify/plugins/back-to-top.js?versionTimeStamp=%PROJECT_VERSION%"></script>');
+    document.write('<script type="text/javascript" src="lib/unify/plugins/smoothScroll.js?versionTimeStamp=%PROJECT_VERSION%"></script>');
+    document.write('<script type="text/javascript" src="lib/payment/sha256.js?versionTimeStamp=%PROJECT_VERSION%"></script>');
+    document.write('<script type="text/javascript" src="lib/payment/sha512.js?versionTimeStamp=%PROJECT_VERSION%"></script>');
+    document.write('<script type="text/javascript" src="lib/jqueryPlugins/imagezoom.js?versionTimeStamp=%PROJECT_VERSION%"></script>');
+    document.write('<script type="text/javascript" src="lib/tinymce/tinymce.min.js?versionTimeStamp=%PROJECT_VERSION%"></script>');
+
+    // <!-- @@dev-js starts -->
+    document.write('<script type="text/javascript" src="lib/require.js" data-main="app/shared/main.js?versionTimeStamp=%PROJECT_VERSION%"></script>');
+    // <!-- @@dev-js ends -->
+    
