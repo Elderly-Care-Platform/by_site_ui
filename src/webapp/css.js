e3284393a9acd9a95a546baa56311a6f60133a18
Aparna <aparna.r@beautifulyears.com>
2016-06-14 11:40:20 +0530

    css changes

--- src/webapp/css.js
+++ src/webapp/css.js
@@ -10,3 +10,19 @@
     document.write('<link rel="stylesheet" href="lib/unify/plugins/flexslider/flexslider.css?versionTimeStamp=%PROJECT_VERSION%">');
     document.write('<link rel="stylesheet" href="lib/unify/plugins/parallax-slider/css/parallax-slider.css?versionTimeStamp=%PROJECT_VERSION%">');
     // <!-- Third party CSS files from libraries end -->
+
+    // Third party CSS files from libraries start 
+    // <link rel="stylesheet" href="lib/unify/plugins/bootstrap/css/bootstrap.min.css?versionTimeStamp=%PROJECT_VERSION%">
+    // <link rel="stylesheet" href="lib/jqueryPlugins/jquery-ui-1.11.4.custom/jquery-ui.min.css?versionTimeStamp=%PROJECT_VERSION%">
+    // <link rel="stylesheet" href="lib/unify/plugins/bootstrap/css/bootstrap-toggle.min.css">
+    // <link rel="stylesheet" href="lib/tagmanager-master/tagmanager.css?versionTimeStamp=%PROJECT_VERSION%">
+    // <link rel="stylesheet" href="lib/unify/plugins/animate.css?versionTimeStamp=%PROJECT_VERSION%">
+    // <link rel="stylesheet" href="lib/unify/plugins/line-icons/line-icons.css?versionTimeStamp=%PROJECT_VERSION%">
+    // <link rel="stylesheet" href="lib/unify/plugins/font-awesome/css/font-awesome.min.css?versionTimeStamp=%PROJECT_VERSION%">
+    // <link rel="stylesheet" href="lib/unify/plugins/flexslider/flexslider.css?versionTimeStamp=%PROJECT_VERSION%">
+    // <link rel="stylesheet" href="lib/unify/plugins/parallax-slider/css/parallax-slider.css?versionTimeStamp=%PROJECT_VERSION%"> 
+    // Third party CSS files from libraries end 
+
+    document.write('<link rel="stylesheet" href="assets/css/final.min.css?versionTimeStamp=%PROJECT_VERSION%">');
+
+
