e04bc7859b61a9e187f6cdd52b37f5b2e414d9c2
Aparna <aparna.r@beautifulyears.com>
2016-06-15 12:21:10 +0530

    css paths

---
+++ src/webapp/lib/replaceFinalCss.js
@@ -0,0 +1,5 @@
+var stylesheet = document.createElement('link');
+stylesheet.href = 'assets/css/final.min.css';
+stylesheet.rel = 'stylesheet';
+stylesheet.type = 'text/css';
+document.getElementsByTagName('head')[0].appendChild(stylesheet);
