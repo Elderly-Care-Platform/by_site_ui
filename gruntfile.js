901c4a8a152165e851aad28bb427952bbe1e61f7
Aparna <aparna.r@beautifulyears.com>
2016-06-15 11:43:00 +0530

    css paths

--- gruntfile.js
+++ gruntfile.js
@@ -178,7 +178,7 @@ module.exports = function (grunt) {
                     patterns: [{
                         match: /\<\!--\s?@@dev-css\s?starts[\s\S]*@@dev-css\s?ends\s?-->/,
                         replacement: function () {
-                            //return '<link rel="stylesheet" href="assets/css/final.min.css?versionTimeStamp=%PROJECT_VERSION%">';
+                            return '<link rel="stylesheet" href="assets/css/final.min.css?versionTimeStamp=%PROJECT_VERSION%">';
                         }
                     },
                     {
